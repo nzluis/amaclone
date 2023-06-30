57679ed6894833316893081dbd289eca06d120cc
luis <luisnavarroFS@gmail.com>
2023-06-30 13:47:18 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>TS Amaclone</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
