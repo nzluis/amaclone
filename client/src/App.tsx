7c2d79047649e2bc9970aaa5db16ac18d03d9159
luis <luisnavarroFS@gmail.com>
2023-08-29 13:52:04 +0200

    list products

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
 
   return (
     <div>
       <header>TS Amaclone</header>
-      <main></main>
+      <main>
+        <ul>
+        {sampleProducts.map(product => (
+        <li key={product.slug}>
+          <img src={product.image} alt={product.name} className='product-image'/>
+          <h2>{product.name}</h2>
+          <p>{product.price}€</p>
+        </li>
+        ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
