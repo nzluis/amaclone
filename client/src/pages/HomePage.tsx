eaa4c5c23eed206e413e53a3d5f5def71e6465c4
luis <luisnavarroFS@gmail.com>
2023-09-13 14:03:02 +0200

    load products by React-Query

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,75 +1,26 @@
 import { Row, Col } from "react-bootstrap"
-import { Link } from 'react-router-dom'
-import { Product } from '../types/Product'
-import { useEffect, useReducer } from 'react'
-import axios from 'axios'
-import { ApiError } from '../types/ApiError'
-import { getError } from '../utils'
 import LoadingBox from '../component/LoadingBox'
 import MessageBox from '../component/MessageBox'
 import ProductItem from "../component/ProductItem"
 import { Helmet } from "react-helmet-async"
+import { useGetProductsQuery } from "../hooks/producHooks"
+import { ApiError } from "../types/ApiError"
+import { getError } from "../utils"
 
-type State = {
-  products: Product[],
-  loading: boolean,
-  error: string
-}
 
-type Action =
-  | { type: 'FETCH_REQUEST' }
-  | {
-      type: 'FETCH_SUCCESS'
-      payload: Product[]
-    }
-  | { type: 'FETCH_FAIL'; payload: string }
-
-  const initialState: State = {
-    products: [],
-    loading: true,
-    error: '',
-  }
-
-  const reducer = (state: State, action: Action) => {
-    switch (action.type) {
-      case 'FETCH_REQUEST':
-        return { ...state, loading: true }
-      case 'FETCH_SUCCESS':
-        return { ...state, products: action.payload, loading: false }
-      case 'FETCH_FAIL':
-        return { ...state, loading: false, error: action.payload }
-      default:
-        return state
-    }
-  }
 
 export default function HomePage() {
-  const [{ loading, error, products }, dispatch] = useReducer<
-    React.Reducer<State, Action>
-  >(reducer, initialState)
-  useEffect(() => {
-    const fetchData = async () => {
-      dispatch({ type: 'FETCH_REQUEST' })
-      try {
-        const result = await axios.get('/api/products')
-        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
-      } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err as ApiError) })
-      }
-    }
-    fetchData()
-  }, [])
-
-  return loading ? (
+  const {data: products, isLoading, error} = useGetProductsQuery()
+  return isLoading ? (
     <LoadingBox />
   ) : error ? (
-    <MessageBox variant="danger">{error}</MessageBox>
+    <MessageBox variant="danger">{getError(error as ApiError)}</MessageBox>
   ) : (
     <Row>
       <Helmet>
         <title>Amaclone</title>
       </Helmet>
-        {products.map(product => (
+        {products!.map(product => (
           <Col key={product.slug} sm={6} md={4} lg={3}>
               <ProductItem product={product} />
           </Col>
