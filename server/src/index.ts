6d7e679a529cfaae6b44efb63bf7f19bde28c89b
luis <luisnavarroFS@gmail.com>
2023-09-15 13:41:06 +0200

    connect to MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,24 @@
 import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
 import { sampleProducts } from './data'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import { productRouter } from './routers/productRouter'
+import { seedRouter } from './routers/seedRouter'
+
+dotenv.config()
+
+const MONGODB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/amaclone'
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch(() => {
+    console.log('error mongodb')
+  })
 
 const app = express()
 app.use(
@@ -10,13 +28,8 @@ app.use(
     })
   )
   
-app.get('/api/products', (req: Request, res: Response) => {
-    res.json(sampleProducts)
-})
-
-app.get('/api/products/:slug', (req: Request, res: Response) => {
-  res.json(sampleProducts.find( x => x.slug === req.params.slug))
-})
+  app.use('/api/products', productRouter)
+  app.use('/api/seed', seedRouter)
 
 const PORT = 4000
 app.listen(PORT, () => {
