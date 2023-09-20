81993517db96a3aed7909321c46405622505a7ff
luis <luisnavarroFS@gmail.com>
2023-09-20 12:22:49 +0200

    create register page

--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -1,5 +1,5 @@
 import express, {Response, Request} from 'express'
-import { UserModel } from '../models/userModel'
+import { User, UserModel } from '../models/userModel'
 import asyncHandler from 'express-async-handler'
 import { generateToken } from '../utils'
 import bcrypt from 'bcryptjs'
@@ -21,5 +21,21 @@ userRouter.post('/signin', asyncHandler(async(req: Request, res: Response) => {
         }
         res.status(401).json({message: 'Invalid email or password'})
     }    
+})
+)
+
+userRouter.post('/signup', asyncHandler(async(req: Request, res: Response) => {
+    const user = await UserModel.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password)
+    } as User)
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      })
 })
 )
