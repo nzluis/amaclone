ca2fac0e65cc7c3f44ffd01087458125e6faacd3
luis <luisnavarroFS@gmail.com>
2023-09-20 13:23:35 +0200

    create payment method page

--- client/src/Store.tsx
+++ client/src/Store.tsx
@@ -43,11 +43,14 @@ type Action =
   | { type: 'USER_SIGNIN'; payload: UserInfo }
   | { type: 'USER_SIGNOUT' }
   | { type: 'SAVE_SHIPPING_ADDRESS'; payload: ShippingAddress }
+  | { type: 'SAVE_PAYMENT_METHOD'; payload: string }
+
 
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
     case 'SWITCH_MODE':
+      localStorage.setItem('mode', state.mode === 'dark' ? 'light' : 'dark')
       return { ...state, mode: state.mode === 'dark' ? 'light' : 'dark' }
     case 'CART_ADD_ITEM':
       const newItem = action.payload
@@ -96,15 +99,20 @@ function reducer(state: AppState, action: Action): AppState {
           totalPrice: 0,
         },
       }
-      case 'SAVE_SHIPPING_ADDRESS':
-        return {
-          ...state,
-          cart: {
-            ...state.cart,
-            shippingAddress: action.payload,
-          },
-        }
-  
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: action.payload,
+        },
+      }
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
+      }
+        
     default:
       return state
   }
