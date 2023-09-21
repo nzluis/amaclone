e5bf0a6fead6f580e541eda058fc0af8bb3fa5a3
luis <luisnavarroFS@gmail.com>
2023-09-21 11:25:40 +0200

    build order create API

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import mongoose from 'mongoose'
 import { productRouter } from './routers/productRouter'
 import { seedRouter } from './routers/seedRouter'
 import { userRouter } from './routers/userRouter'
+import { orderRouter } from './routers/orderRouter'
 
 dotenv.config()
 
@@ -34,6 +35,7 @@ app.use(
   
   app.use('/api/products', productRouter)
   app.use('/api/users', userRouter)
+  app.use('/api/orders', orderRouter)
   app.use('/api/seed', seedRouter)
 
 const PORT = 4000
